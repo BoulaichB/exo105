07c33c9cf99447c5e9fae0a5c0e634ecb514ed7b
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 10:16:01 +0100

    Background bleu au focus et blanc au focus out du deuxieme input

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 let input = document.getElementsByTagName('input')[0];
-
 input.addEventListener('focus', () =>{
     input.style.backgroundColor = 'blue';
+})
+
+let input2 = document.getElementsByTagName('input')[1];
+input2.addEventListener('focus', () =>{
+    input2.style.backgroundColor = 'blue';
+});
+input2.addEventListener('focusout', () =>{
+    input2.style.backgroundColor = 'white';
 })
